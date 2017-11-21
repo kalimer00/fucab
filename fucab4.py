d9330ba68f247d23898ca3455352455f89bc05e5
kalimer00 <32965279+kalimer00@users.noreply.github.com>
2017-11-21 22:18:06 +0100

    Add files via upload

--- fucab4.py
+++ fucab4.py
@@ -1,11 +1,34 @@
-import sys, funktionen, xlrd, os
-from random import randrange
-from funktionen import auswahlInt
+# -*- coding: utf-8 -*-
+import sys, funktionen, os
+from funktionen import auswahl
 
-list_dir = os.listdir("data")
+# from funktionen import auswahlInt
 
-funktionen.auswahlInt("-------Inhalt des Ordners------", *list_dir, "Welche Datei?: ")
-print(type(funktionen.auswahl))
+waehlen = "Bitte auswählen: "
+list_dir = os.listdir("excel")
 
-sheet_names = funktionen.excelSheet()
-print(' | ' . join(sheet_names))
+funktionen.header()
+for dateien in list_dir:
+    if not (dateien.endswith(".xlsx") or dateien.endswith(".xls")):
+        print("Datei ist keine Excel-Datei!")
+    else:
+        funktionen.auswahlInt("-------Inhalt des Ordners------", list_dir, "Welche Datei?: ")
+
+        sheet_names = funktionen.excelSheet()
+        sheet_mit_style = ' | '.join(sheet_names)
+        funktionen.auswahlInt("-------Seite / Sheet (Thema)---", sheet_mit_style, waehlen)
+        funktionen.readExel()
+        funktionen.auswahlInt("-------Spalte Wählen (Sprache)-", "1) Erste Spalte 2) Zweite Spalte 3) Beenden", waehlen)
+        if funktionen.auswahl == "1":
+            funktionen.zufall(1)
+            funktionen.aufloesung()
+        elif funktionen.auswahl == "2":
+            print("Zahl 2")
+            funktionen.zufall(0)
+            funktionen.aufloesung()
+        elif funktionen.auswahl == "3":
+            funktionen.aufloesung()
+            sys.exit()
+        else:
+            print("Auswahl existiert nicht!")
+            sys.exit()
--- funktionen.py
+++ funktionen.py
@@ -1,101 +1,100 @@
-import os, xlrd, sys
+# -*- coding: utf-8 -*-
+import sys
+import xlrd
 from random import randrange
-################################ header ###################################
+
+# -------------------------------- header --------------------------------
 def header():
-	try:
-		print("")
-		print("**************************************************************************************")
-		print("+                                                                                    +")
-		print("+      88888888888  888    888     8888888888      88888         888888888           +")
-		print("+      8888888888   888    888    888    888      888 888        888     88          +")
-		print("+      8888         888    888    888            888   888       888    88           +")
-		print("+      8888         888    888   888            888     888      8888888>            +")
-		print("+      888888888    888    888   888           8888888888888     888    88           +")
-		print("+      8888         8888888888  888    888   888          888    888     88          +")
-		print("+      8888          88888888   8888888888  88888        88888  8888888888    V1.0   +")
-		print("+                                                                                    +")
-		print("**************************************************************************************")
-		print("")
-	except Exception:
-		print("Fehler in Funktion header()!")
-		sys.exit()
-################################ Dateien auflisten ###################################
-def welcheDatei():
-	global exl_datei
-	global list_dir
+    try:
+        print("")
+        print("**************************************************************************************")
+        print("+                                                                                    +")
+        print("+                              FUCAB VocabelReader V1.2                              +")
+        print("+     Liest zwei Spalten einer Excel-Datei ein und gibt zufällig eine Zelle aus.     +")
+        print("+                                                                                    +")
+        print("**************************************************************************************")
+        print("")
+    except ValueError:
+        print("Fehler in Funktion header()!")
+        sys.exit()
 
-	try:
-		list_dir = os.listdir("data")
-		print("----------------------------------    Excel auswählen    -----------------------------")
-		print(*list_dir)
-		print("")
-		exl_datei = input("Welche Datei?: ")
-	except Exception:
-		print("Fehler in Funktion welcheDatei()!")
-		sys.exit()
-################################ Excel Seite wählen ###################################
-def welchesSheet():
-	global exl_sheet
-	global sheet_names
-	global workb
 
-	try:
-		file_path = "data/"+exl_datei
-		wb = xlrd.open_workbook(file_path)
-		workb = wb.sheet_by_index(0)
-		sheet_names = wb.sheet_names()
+# -------------------------------- Auswahl generell --------------------------------
+# text und text2 sind Ausgabefelder, todo ist der Text für die Eingabe (input())
 
-		# Excel Sheet bzw. Seite auswählen
-		print("")
-		print("-------------------------------    Exel Sheets/Seiten    -----------------------------")
-		print(' | ' . join(sheet_names))
-		print("")
-		exl_sheet = input("Welches Thema?: ")
-	except Exception:
-		print("Fehler in Funktion welchesSheet!()")
-		sys.exit()
-################################ excel einlesen ###################################
-def readExel():
-	global data
-	data = [[workb.cell_value(r, c) for c in range(workb.ncols)] for r in range(workb.nrows)]
-################################ Spalte wählen #####################################
-def spWaehlen():
-	global sprache
-	try:
-		sprache = int(input("1) Deutsch,  2) Englisch oder 3) Beenden: "))
-	except Exception:
-		print("Fehler in der Sprachauswahl!")
-################################ Auswahl generell #####################################
-# text und text2 sind Ausgabefelder, todo ist ein Eingabetext der übernommen wird
+auswahl = None
+
 def auswahlInt(text, text2, todo):
-	global auswahl
-	try:
-		print("")
-		print(text)
-		print(text2)
-		print("")
-		auswahl = input(todo)
-		if auswahl.isdigit() == True:
-			print("Ist Int: "+auswahl)
-			auswahl = int(auswahl)
-			print("Auswahl wurde zu Integer Umgewandelt!")
-			return auswahl
-		else:
-			print("Ist String: "+auswahl)
-			return auswahl
-	except:
-		print("Fehler in der generellen Auswahl!")
-		sys.exit()
-################################ Excel Sheet / Seite ##################################
-def excelSheet():
-	#global sheet_names
-	try:
-		file_path = "data/"+auswahl
-		wb = xlrd.open_workbook(file_path)
-		workb = wb.sheet_by_index(0)
-		sheet_names = wb.sheet_names()
+    global auswahl
+    try:
+        print("")
+        print(text)
+        print(text2)
+        print("")
+        auswahl = input(todo)
+        #if auswahl.isdigit():
+            #print("Nummer " + auswahl + " wurde ausgewählt")
+            #auswahl = int(auswahl)
+            # print("Auswahl wurde zu Integer Umgewandelt!")
+        #else:
+            #print("Auswahl: " + auswahl)
+    except ValueError:
+        print("Fehler in der generellen Auswahl!")
+        sys.exit()
 
-		return sheet_names
-	except:
-		print("Fehler in Funktion excelSheet()!")
-		sys.exit()
+
+# -------------------------------- Excel Sheets / Seiten anzeigen --------------------------------
+def excelSheet():
+    global workb
+    try:
+        file_path = "excel/" + auswahl
+        wb = xlrd.open_workbook(file_path)
+        workb = wb.sheet_by_index(0)
+        sheet_names = wb.sheet_names()
+
+        return sheet_names
+    except ValueError:
+        print("Fehlerhafte Eingabe!")
+        sys.exit()
+
+
+# -------------------------------- Excel Daten einlesen --------------------------------
+def readExel():
+    global data
+    data = [[workb.cell_value(r, c) for c in range(workb.ncols)] for r in range(workb.nrows)]
+    return data
+
+
+# -------------------------------- Spalte wählen --------------------------------
+def spWaehlen():
+    global sprache
+    try:
+        sprache = int(input("1) Deutsch,  2) Englisch oder 3) Beenden: "))
+    # return sprache
+    except ValueError:
+        print("Fehler in der Sprachauswahl!")
+
+
+# -------------------------------- Zufallsvokabel --------------------------------
+def zufall(x):
+    global zufall
+    zufall = randrange(0, len(readExel()))
+    zuffallswort = 0
+    zuffallswort = str(data[zufall][x])
+
+    print("")
+    print("--------------------------------------------------------------")
+    print("Gib bitte die Übersetzung von | " + zuffallswort + " | ein.")
+    print("--------------------------------------------------------------")
+    print("")
+    aufloesung = input("Drücke 1 für die Auflösung oder 2 für die Eingabe: ")
+    return aufloesung
+
+
+# -------------------------------- Auflösung --------------------------------
+def aufloesung():
+    print("")
+    print("################################################################")
+    print("   " + data[zufall][0] + " - bedeutet - " + data[zufall][1])
+    print("################################################################")
+    print("")
